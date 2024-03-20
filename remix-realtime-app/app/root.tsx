faa33ed230a739e2653149834de0347b1df33c5c
Vincent-Carlson <vincar@student.chalmers.se>
2024-03-20 12:26:22 +0100

    added example realtime-app and resolved some deprecations to make it run

---
+++ remix-realtime-app/app/root.tsx
@@ -0,0 +1,50 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useRevalidator,
+} from "@remix-run/react";
+import { useEffect } from "react";
+import { useEventSource } from "remix-utils";
+
+import icons from "~/icons.svg";
+import styles from "~/styles.processed.css";
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Remix Fake Linear Demo",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  { rel: "preload", href: icons, as: "image", fetchpriority: "high" },
+];
+
+export default function App() {
+  useRealtimeIssuesRevalidation();
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+function useRealtimeIssuesRevalidation() {
+  const data = useEventSource("/issues-events");
+  const { revalidate } = useRevalidator();
+  useEffect(() => {
+    revalidate();
+  }, [data, revalidate]);
+}
